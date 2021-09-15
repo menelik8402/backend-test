305a5774fe05aa1eebb929612e6558adfb46e812
menelik8402 <menelik.mesa@gmail.com>
2021-09-15 01:46:21 -0400

    Se agrego la ruta correspondiente listado de informe del sistema

--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,4 @@ Route::post('/save/emails','App\Http\Controllers\EmailautomatedController@store'
 Route::get('/all','App\Http\Controllers\VehiculoController@index');
 Route::post('/entrada/vehiculo','App\Http\Controllers\RegistroVehController@store');
 Route::put('/salida/vehiculo','App\Http\Controllers\RegistroVehController@updateVehicleOut');
+Route::get('/informe/clientes','App\Http\Controllers\RegistroVehController@index');
